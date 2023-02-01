7614425dff1cdf74984736c2068f16fa98311311
V4NT4 <ivanta788@gmail.com>
2023-02-01 09:03:46 +0700

    awalan

---
+++ menu.php
@@ -0,0 +1,58 @@
+<link rel="stylesheet" href="style.css">
+<div class="menu">
+<nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          
+          <li class="nav-header">EXAMPLES</li>
+          <li class="nav-item">
+            <a href="index.php" class="nav-link">
+              <i class="nav-icon fa fa-user"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fa fa-user"></i>
+              <p>
+                Admin
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fa fa-user"></i>
+              <p>
+                Owner
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-user"></i>
+              <p>
+                Operator
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-user"></i>
+              <p>
+                Pelanggan
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="kasir.php" class="nav-link">
+              <i class="nav-icon fas fa-cash-register"></i>
+              <p>
+                Kasir
+              </p>
+            </a>
+          </li>
+      </nav>
+      </div>
